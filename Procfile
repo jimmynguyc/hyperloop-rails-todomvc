5cf5a8055f74797c65baab11ead93623e4787670
Jimmy Ngu <jimmynguyc@gmail.com>
2018-09-03 00:11:06 +0800

    Opal Hot Reload

---
+++ Procfile
@@ -0,0 +1,2 @@
+web:        bundle exec rails s -p3000
+hot-loader: bundle exec opal-hot-reloader -d app/hyperloop
--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -3,5 +3,6 @@
 Hyperloop.configuration do |config|
   config.transport = :action_cable
   config.import 'reactrb/auto-import'
+  config.import 'opal_hot_reloader' if Rails.env.development?
 end
 
