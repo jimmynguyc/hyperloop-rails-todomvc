3645c97068be4407e6d601484fa6fd12d79b4a8a
Jimmy Ngu <jimmynguyc@gmail.com>
2018-09-04 03:16:55 +0800

    List Item

--- app/hyperloop/components/list.rb
+++ app/hyperloop/components/list.rb
@@ -4,21 +4,8 @@ class List < Hyperloop::Component
       INPUT(id: "toggle-all", class:"toggle-all", type:"checkbox")
       LABEL(htmlFor: "toggle-all") { 'Mark all as complete' }
       UL(class: 'todo-list') do
-        LI(class: 'completed') do
-          DIV(class: 'view') do
-            INPUT(class: 'toggle', type: 'checkbox', defaultChecked: true)
-            LABEL { ' Taste JavaScript' }
-            BUTTON(class: 'destroy')
-          end
-          INPUT(class: 'edit', defaultValue: 'Create a TodoMVC template')
-        end
-        LI do
-          DIV(class: 'view') do
-            INPUT(class: 'toggle', type: 'checkbox', defaultChecked: false)
-            LABEL { ' Buy a unicorn' }
-            BUTTON(class: 'destroy')
-          end
-          INPUT(class: 'edit', defaultValue: 'Rule the web')
+        Todo.each do |item|
+          ListItem(item: item)
         end
       end
     end
---
+++ app/hyperloop/components/list_item.rb
@@ -0,0 +1,14 @@
+class ListItem < Hyperloop::Component
+  param :item
+
+  render do
+    LI(class: params.item.completed ? 'completed' : '') do
+      DIV(class: 'view') do
+        INPUT(class: 'toggle', type: 'checkbox', checked: params.item.completed)
+        LABEL { params.item.title }
+        BUTTON(class: 'destroy')
+      end
+      INPUT(class: 'edit', defaultValue: params.item.title)
+    end
+  end
+end
